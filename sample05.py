4cc65ab09f9cb394d7526ec6618cafc6a4cd46b0
Kudo <kudo@example.com>
2019-03-10 12:06:43 +0900

    importを使って関数を呼び出す

--- sample05.py
+++ sample05.py
@@ -4,9 +4,6 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-y1 = add(a=3, b=4)
-
-print(y1)
 
 
 # 2分課題: 2つの整数の差を返す関数を作る
@@ -15,6 +12,12 @@ def sub(a: int, b: int) -> int:
     return a - b
 
 
-y1 = sub(a=3, b=4)
+# if以下のみsample05でtureなので表示される。その他では
+if __name__ == "__main__":
+    y1 = add(a=3, b=4)
 
-print(y1)
+    print(y1)
+
+    y1 = sub(a=3, b=4)
+
+    print(y1)
---
+++ sample06.py
@@ -0,0 +1,11 @@
+import sample05
+
+y = sample05.add(a=15, b=7)
+
+print(y)  # 22が出力されて欲しい
+
+import sample05
+
+y2 = sample05.sub(a=33, b=18)
+
+print(y2)
