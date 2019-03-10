4c27c8193717fe24a231135adbd5d08e2850047f
Kudo <kudo@example.com>
2019-03-10 11:49:00 +0900

    2つの整数の差を返す関数の追加

--- sample05.py
+++ sample05.py
@@ -7,3 +7,14 @@ def add(a: int, b: int) -> int:
 y1 = add(a=3, b=4)
 
 print(y1)
+
+
+# 2分課題: 2つの整数の差を返す関数を作る
+
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+y1 = sub(a=3, b=4)
+
+print(y1)
